f640d0b5490bf80faa806386db3d38e29e2a29a3
SwoosOfficial <swoosdontknowme@gmail.com>
2014-12-21 20:27:33 +0100

    findBra now correct!

--- debug.py
+++ debug.py
@@ -1,127 +1,95 @@
-def checkDiv(func):
-	if "/" in func:
-		lastOcc=0;
-		while lastOcc != -1:
-			checkFuncList = {};
-			if "/(" in func:
-				checkFunc=func[func.find("/(",lastOcc +1)+2:findBra(func,func.find("/(",lastOcc +1)) +1];
-				checkFuncList[lastOcc]=checkFunc;
-			else:
-				checkFunc=func[func.find("/")+1];
-				checkFuncList[lastOcc]=checkFunc;
-			lastOcc=func.find("/",lastOcc +1);
-#	elif "**(" or "**" in func:
-#		lastOcc=0;
-#		while lastOcc != -1:
-#			checkFuncList = {};
-#			if ")**(-1)" in func or ")**-1":
-#				checkfunc=func[
 opList=["+","-","/","*","|","^","%","&","_"];
 alphaList=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
-braList=["(",")","[","]","{","}","«","»","„","“"];
+braList=["(","[","{","«","„",")","]","}","»","“"];
+braOpenList=braList[:5];
+braCloseList=braList[5:];
 dotList=[".",","];
-intLIst=["0","1","2","3","4","5","6","7","8","9"]
-def corFunc(func):
+intList=["0","1","2","3","4","5","6","7","8","9"];
+funcDepth=-1;
+								
+def getSubFunc(func):
 	n=0;
+	wordEndPos=0;
+	wordEndPosList=[];
 	wordList=[]
-	wordEndPos=0
 	while n < len(func):
-		getWord(func,wordEndPos,true,true);
-		n+=1;
-		n=max([n,wordEndPos]);
-
-	checkPos=0;
-	lastPosType="op";
-	while checkPos < len(func):
 		try:
-			int(func[checkPos]);
-			if lastPosType=="str":
-				func=injectString(func,"*",checkPos);
-			lastPosType="int";
-			checkPos+=1;
-		except ValueError:
-			if func[checkPos] in dotList:
-				if lastPosType == "op" or lastPosType == "bra":
-					func=injectString(func,"0.",checkPos,1)
-				elif lastPosType == "int" or lastPosType == "str":
-					func=injectString(func,".",checkPos,1);
-				elif lastPosType == "dot":
-					func=ignoreString(func,checkPos);
-			elif func[checkPos] in opList:
-				if lastPosType == "op":
-					if func[checkPos] == "+":
-						func=ignoreString(func,checkPos)
-					elif func[checkPos] == "-":
-						if checkPos > 0:
-							if func[checkPos-1] == "+":
-								func=ignoreString(func,checkPos-1);
-							elif func[checkPos-1] == "-":
-								func=injectString(func,"+",checkPos-1,2);
-							elif func[checkPos-1] == "/":
-								if func[checkPos+1] == "(":
-									varBraPos=findBra(func,checkPos+1);
-									varInj="((-1)*"+func[checkPos+1:varBraPos+1]+")"
-									func=injectString(func,varInj,checkPos,,varBraPos+1);
-								elif func[checkPos+1] in intList:
-									varInj="(-"+func[checkPos+1]+")";
-									func=injectString(func,varInj,checkPos,1);
-								elif func[checkPos+1] in alphaList +dotList
-									
+                        if getWord(func,n,1)[0] not in ["x","y","z","."]:
+                                wordList.append(getWord(func,n,1)[0]);
+                                wordEndPos=getWord(func,n,1)[1];
+                                wordEndPosList.append(wordEndPos);
+		except TypeError:
+			pass
+		finally:
+			if wordEndPos>n:
+				n=wordEndPos;
+			else:
+				n+=1;
+	return [wordList,wordEndPosList];
+
+def getArgs(posList):
+        n=0;
+        argList=[];
+        while n<len(posList) and posList[n]<len(func):
+                nextPosVal=func[posList[n]];
+                if nextPosVal in intList:
+                        argList.append(nextPosVal);
+                elif nextPosVal in opList:
+                        argList.append("x");
+                elif nextPosVal in braList:
+                        arg=func[posList[n]+1:findBra(func,posList[n])];
+                        argList.append(arg);
+                else:
+                        print("Fatal Error in getArgs"+repr(n)+nextPosVal);
+                n+=1;
+        return argList;
+
+
 def injectString(string,injection,start,ignore=0,end=0):
 	if end == 0:
 		end=ignore+start;
 	string=string[:start]+injection+string[end:];
 	return string;
+
 def ignoreString(string,start,ignore=1):
 	end=start+ignore;
 	string=string[:start]+string[end:];
 	return string;
 
-def getWord(string,pos=0,dot=false,global=false):
-	word="";
+def getWord(string,pos=0,dot=0):
 	if dot:
-		try:
-			while string[pos] in alphaList +dotList:
+		if len(string)>pos and string[pos] in alphaList +dotList:	
+			word="";
+			while len(string)>pos and string[pos] in alphaList + dotList:
 				word+=string[pos];
 				pos+=1;
-		except IndexError:
-			print("Hallo");
+			return [word,pos];
 	else:
-		try:
+		if len(string)>pos and string[pos] in alphaList:	
+			word="";
 			while string[pos] in alphaList:
 				word+=string[pos];
 				pos+=1;
-		except: IndexError
-			print("Hallo");
-	return word;
+			return [word,pos];
+def findBra(string, braPos):
+        braType=string[braPos];
+        try:
+                braListPos=braList.index(braType);
+        except ValueError:
+                print(braType+" is not a bracket.");
+        if braListPos<5:
+                endPos=braPos;
+                matchBra=braCloseList[braListPos];
+                while string.count(matchBra, braPos, endPos+1)!=string.count(braType, braPos, endPos+1):
+                        endPos+=1;
+                return endPos;
+        else:
+                startPos=braPos;
+                matchBra=braOpenList[braListPos-5];
+                while string.count(matchBra, startPos, braPos+1)!=string.count(braType, startPos, braPos+1):
+                       startPos-=1;
+                return startPos;
 
-def findBra(string,braPos, braType="("):
-	countBra=0;
-	varBraPos=braPos;
-	countSearch=0;
-	if braType == "(":
-		while string.find("(",varBraPos+1) < string.find(")",varBraPos+1) and string.find("(",varBraPos+1)!=-1:
-			countBra=countBra +1;
-			varBraPos=string.find("(",varBraPos+1);
-		varBraPos=braPos
-		while countBra >= countSearch:
-			varBraPos=string.find(")",varBraPos+1);
-			countSearch=countSearch+1;
-		return varBraPos;
-	elif braType == ")":
-		revString=string[::-1];
-		varBraPos=revBraPos=len(string)-1-braPos;
-		while revString.find(")",varBraPos+1) < revString.find("(", varBraPos+1) and revString.find(")",varBraPos+1)!=-1:
-			countBra=countBra +1;
-			varBraPos=revString.find(")",varBraPos+1);
-		varBraPos=revBraPos;
-		while countBra >= countSearch:
-			varBraPos=revString.find("(",varBraPos+1);
-			countSearch=countSearch+1;
-		varBraPos=len(string)-1-varBraPos;
-		return varBraPos;
-	else:
-		raise NameError("BraType is invalid");
-a="fuhuhuhuhu"
-b=injectString(a,"bla",2)
-print(b)
+func="2*(sin(2*(cos(222)))+cosh(5)+cos(3.8))"
+print(func)
+print(getArgs(getSubFunc(func)[1]));
--- debug2.py
+++ debug2.py
@@ -2,59 +2,117 @@ opList=["+","-","/","*","|","^","%","&","_"];
 alphaList=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 braList=["(",")","[","]","{","}","«","»","„","“"];
 dotList=[".",","];
-intLIst=["0","1","2","3","4","5","6","7","8","9"]
-def getSubFunc(func):
-	n=0;
-	wordList=[]
-	wordEndPos=0
-	while n < len(func):
-		try:
-			wordList.append(getWord(func,n,1)[0]);
-			wordEndPos=getWord(func,n,1)[1];
-		except TypeError:
-			pass
-		finally:
-			if wordEndPos>n:
-				n=wordEndPos;
-			else:
-				n+=1;
-	return wordList;
+intList=["0","1","2","3","4","5","6","7","8","9"];
 
 def corSubFunc(func):
 	n=0;
-	wordList = getSubFunc(func);
+	wordList = getSubFunc(func)[0];
+	wordPosList = getSubFunc(func)[1];
 	while n < len(wordList):
 # Functions
+#       Trigonometric:
 		if wordList[n] in sinList:
+                        print("Interpreting "+wordList[n]+"as sine function (numpy.sin).");
+                        wordList[n] = "numpy.sin";
 		elif wordList[n] in cosList:
+                        print("Interpreting "+wordList[n]+"as cosine function (numpy.cos).");
+                        wordList[n] = "numpy.cos";
 		elif wordList[n] in tanList:
-		elif wordList[n] in logList:
-		elif wordList[n] in expList:
-		elif wordList[n] in sgnList:
-		elif wordList[n] in absList:
+                        print("Interpreting "+wordList[n]+"as tangent function (numpy.tan).");
+                        wordList[n] = "numpy.tan";
 		elif wordList[n] in arccosList:
+                        print("Interpreting "+wordList[n]+"as inverse cosine function (numpy.arccos).");
+                        wordList[n] = "numpy.arccos";
 		elif wordList[n] in arcsinList:
+                        print("Interpreting "+wordList[n]+"as inverse sine function (numpy.arcsin).");
+                        wordList[n] = "numpy.arcsin";
 		elif wordList[n] in arctanList:
+                        print("Interpreting "+wordList[n]+"as inverse tangent function (numpy.arctan).");
+                        wordList[n] = "numpy.arctan";
+                elif wordList[n] in cotList:
+                        print("Interpreting "+wordList[n]+"as cotangent function (1/numpy.tan).");
+                        wordList[n] = "1/numpy.tan";
+		elif wordList[n] in cscList:
+                        print("Interpreting "+wordList[n]+"as cosecant function (1/numpy.sin).");
+                        wordList[n] = "1/numpy.sin";
+		elif wordList[n] in secList:
+                        print("Interpreting "+wordList[n]+"as secant function (1/numpy.cos).");
+                        wordList[n] = "1/numpy.cos";
+                elif wordList[n] in arccotList:
+                        print("Interpreting "+wordList[n]+"as inverse cotangent function (numpy.arctan(reciprocal()).");
+                        wordList[n] = "numpy.arctan(numpy.reciprocal())";
+                elif wordList[n] in arccscList:
+                        print("Interpreting "+wordList[n]+"as inverse cosecant function (numpy.arcsin(reciprocal()).");
+                        wordList[n] = "numpy.arcsin(numpy.reciprocal())";
+                elif wordList[n] in arcsecList:
+                        print("Interpreting "+wordList[n]+"as inverse secant function (numpy.arccos(reciprocal()).");
+                        wordList[n] = "numpy.arccos(numpy.reciprocal())";
+#       Hyperbolic:
 		elif wordList[n] in coshList:
+                        print("Interpreting "+wordList[n]+"as cosine hyperbolicus function (numpy.cosh).");
+                        wordList[n] = "numpy.cosh";
 		elif wordList[n] in sinhList:
+                        print("Interpreting "+wordList[n]+"as sine hyperbolicus function (numpy.sinh).");
+                        wordList[n] = "numpy.sinh";
 		elif wordList[n] in tanhList:
-		elif wordList[n] in 
+                        print("Interpreting "+wordList[n]+"as tangent hyperbolicus function (numpy.tanh).");
+                        wordList[n] = "numpy.tanh";
+                elif wordList[n] in arccoshList:
+                        print("Interpreting "+wordList[n]+"as inverse cosine hyperbolicus function (numpy.arccosh).");
+                        wordList[n] = "numpy.arccosh";
+                elif wordList[n] in arcsinhList:
+                        print("Interpreting "+wordList[n]+"as inverse sine hyperbolicus function (numpy.arcsinh).");
+                        wordList[n] = "numpy.arcsinh";
+                elif wordList[n] in arctanhList:
+                        print("Interpreting "+wordList[n]+"as inverse tangent hyperbolicus function (numpy.arctanh).");
+                        wordList[n] = "numpy.arctanh";
+                elif wordList[n] in cotanhList:
+                        print("Interpreting "+wordList[n]+"as cotangent hyperbolicus function (coth).");
+                        wordList[n] = "1+(2)/(-1+numpy.exp(2*()))";
+                elif wordList[n] in sechList:
+                        print("Interpreting "+wordList[n]+"as secant hyperbolicus function (1/numpy.cosh).");
+                        wordList[n] = "1/numpy.cosh";
+                elif wordList[n] in cschList:
+                        print("Interpreting "+wordList[n]+"as cossecant hyperbolicus function (csch).");
+                        wordList[n] = "(2*numpy.exp())/(-1+numpy.exp(2*()))";
+                elif wordList[n] in arccotanhList:
+                        print("Interpreting "+wordList[n]+"as inverse cotangent hyperbolicus function (arccoth).");
+                        wordList[n] = "numpy.arctanh(numpy.reciprocal())";
+                elif wordList[n] in arcsechList:
+                        print("Interpreting "+wordList[n]+"as inverse secant hyperbolicus function (arcsech).");
+                        wordList[n] = "numpy.arccosh(numpy.reciprocal())";
+                elif wordList[n] in arccschList:
+                        print("Interpreting "+wordList[n]+"as inverse cosecant hyperbolicus function (arccsch).");
+                        wordList[n] = "numpy.arcsinh(numpy.reciprocal())";
+#       Exponents and Logarithms:
+                elif wordList[n] in logList:
+                        print("Interpreting "+wordList[n]+"as natural logarithm (numpy.log)");
+                        wordList[n] = "numpy.log";
+		elif wordList[n] in expList:
+                        print("Interpreting "+wordList[n]+"as exponential function (numpy.exp)");
+                        wordList[n] = "numpy.exp";
+#       Misc:
+		elif wordList[n] in sgnList:
+                        print("Interpreting "+wordList[n]+"as sign function (numpy.sign)");
+                        wordList[n] = "numpy.sign";
+		elif wordList[n] in absList:
+                        print("Interpreting "+wordList[n]+"as absolute value function (numpy.absolute)");
+                        wordList[n] = "numpy.absolute";        
+		elif wordList[n] in rootList:
+                        print("Interpreting "+wordList[n]+"as root function (sqrt)");
+                        wordList[n] = "numpy.sqrt";
+#                       only square root yet
+                elif wordList[n] in minList:
+                        print("Interpreting "+wordList[n]+"as minimum value function (numpy.minimum)");
+                        wordList[n] = "numpy.minimum";
+                elif wordList[n] in maxList:
+                        print("Interpreting "+wordList[n]+"as maximun value function (numpy.maximum)");
+                        wordList[n] = "numpy.maximum";
+                elif wordList[n] in gaussList:
 # Constants
-def getWord(string,pos=0,dot=0):
-	if dot:
-		if len(string)>pos and string[pos] in alphaList +dotList:	
-			word="";
-			while len(string)>pos and string[pos] in alphaList + dotList:
-				word+=string[pos];
-				pos+=1;
-			return [word,pos];
-	else:
-		if len(string)>pos and string[pos] in alphaList +dotList:	
-			word="";
-			while string[pos] in alphaList:
-				word+=string[pos];
-				pos+=1;
-			return [word,pos];
-
-corFunc("sadjfk.f34k2dj28d")
-print(len("sadjfk.f34k2dj28d"))
+                elif wordList[n] in piList:
+                        print("Interpreting "+wordList[n]+"as pi ("+scipy.pi+")");
+                        wordList[n] = "scipy.pi";
+                elif wordList[n] in goldenList:
+                        print("Interpreting "+wordList[n]+"as golden ratio ("+scipy.golden+")");
+                        wordList[n] = "scipy.golden";
